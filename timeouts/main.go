e709afd8596aef6d5110391e2faf43c5d348a032
Yinghua Chai <yinghuachai0@gmail.com>
2023-03-01 22:59:56 +0800

    chore: learn basics from go by example

---
+++ timeouts/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func timeoutExample() {
+ c1 := make(chan string, 1)
+    go func() {
+        time.Sleep(2 * time.Second)
+        c1 <- "result 1"
+    }()
+
+    select {
+    case res := <-c1:
+        fmt.Println(res)
+    case <-time.After(1 * time.Second):
+        fmt.Println("timeout 1")
+    }
+}
+
+func resultExample() {
+ c2 := make(chan string, 1)
+    go func() {
+        time.Sleep(2 * time.Second)
+        c2 <- "result 2"
+    }()
+    select {
+    case res := <-c2:
+        fmt.Println(res)
+    case <-time.After(3 * time.Second):
+        fmt.Println("timeout 2")
+    }
+}
+
+func main() {
+	timeoutExample()
+	 resultExample()
+   
+}
